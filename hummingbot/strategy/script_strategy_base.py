12325c71aae60ea5947e8477524228ea47b75991
sdcgvhgj <sdcgvhgj@gmail.com>
2025-09-18 05:11:00 +0000

    change trading-pairs print log

--- hummingbot/strategy/script_strategy_base.py
+++ hummingbot/strategy/script_strategy_base.py
@@ -70,10 +70,10 @@ class ScriptStrategyBase(StrategyPyBase):
             if not self.ready_to_trade:
                 for con in [c for c in self.connectors.values() if not c.ready]:
                     self.logger().warning(f"{con.name} is not ready due to {con.not_ready_reason()}. Please wait...")
-                # Print concerned info if connectors are first ready to trade
-                for connector in self.connectors.values():
-                    self.logger().debug(f"Trading-pairs for {connector.name}: {connector.trading_rules.keys()}")
                 return
+            # Print concerned info if connectors are first ready to trade
+            for connector in self.connectors.values():
+                self.logger().debug(f"Trading-pairs for {connector.name}: {','.join(connector.trading_pairs)}")
         else:
             self.on_tick()
 
